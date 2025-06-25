37cda63c610ba6433ace9f6141962a695de76ffa
Danny Nguyen <danny@MacBook-Pro-cua-Danny.local>
2025-06-25 14:28:48 +0700

    first commit

---
+++ src/main/java/com/example/test_sonarqube/TestSonarqubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_sonarqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSonarqubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSonarqubeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/test_sonarqube/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.test_sonarqube.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    // This is a test controller for SonarQube analysis
+    // It contains a method that returns a simple greeting message
+    @GetMapping("/greet")
+    public String greet() {
+        String greeting = "Hello, SonarQube!";
+        return "Hello, SonarQube!";
+    }
+}
